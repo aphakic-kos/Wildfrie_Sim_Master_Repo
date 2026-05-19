88d736a5d2944baa796ade13e0f4b9a421849a2a
Jai Dunne <jaidunne12@gmail.com>
2026-05-19 14:56:45 +0200

    Added comments for readability of my method

--- src/core/engine.py
+++ src/core/engine.py
@@ -6,14 +6,12 @@ import board
 import rules
 
 
-# MAP GENERATION
-
-#First step is the initialisation of the grid size 
-
+# MAP GENERATION #
 class MapGeneration(board.MapCreator):
-
+# Initialises the board module and its inputs
     def __init__(self, seed, num_rows, num_cols, path, output):
-        self.gen_type = gen_type
+        self.seed = seed
+# Creates a new input called seed which can be used to generate a predefined map grid
         super(MapGeneration).__init__(num_rows, num_cols)
         if self.seed == 'random':
             self.random_map()
@@ -22,20 +20,20 @@ class MapGeneration(board.MapCreator):
         else:
             raise ValueError('invalid seed input')
         self.save_data(path, output)
-
+# A method for adding barriers to be used with GUI, can be used to skip the map genration and load a pre generated map instead
     def add_barriers(self, filename, row, col, path, output)
         super(MapGeneration).__init__(fiename)
         self.build_barrier(row, col)
         self.save_data(path, output)
 
-
+# SIMULATION #
 class Simuation(board.Map, rules.Rules):
+# Initialises the methods needed from board and rules
     def __init__(self, total_steps, filename, grid)
-        self.total_steps = total_steps
         super(Simulation).__init__(filename)
         super(Simulation).__init__(grid)
 
-
+# Method for setting the ignition point for the sim 
     def run(self, ignition_point):
         self.ignition_row = ignition_point[0]
         self.ignition_col = ignition_point[1]
@@ -44,11 +42,11 @@ class Simuation(board.Map, rules.Rules):
         self.ignite_fire(self.ignition_row, self.ignition_col)
         self.fire_row = self.iginition_row
         self.fire_col = self.ignition_col
-
+# The main loop which is automatically started after ignition
         while current_step <= total_steps:
-            self.probibility(self.fire_row, self.fire_col)
+            self.new_cell(self.fire_row, self.fire_col)
             for idx in grid:
-
+                
 
 
             
